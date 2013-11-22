bed25f071f9b76a297f2e6f6656a643e48d5b932
dezzie <dezzie.zamora@gmail.com>
2013-11-22 14:04:27 -0500

    styled tables a bit

--- console/swagger-ui.js
+++ console/swagger-ui.js
@@ -334,7 +334,7 @@ function program3(depth0,data) {
 function program5(depth0,data) {
   
   
-  return "\n          <h4>Parameters</h4>\n          <table class='fullwidth'>\n          <thead>\n            <tr>\n            <th style=\"width: 100px; max-width: 100px\">Parameter</th>\n            <th style=\"width: 310px; max-width: 310px\">Value</th>\n            <th style=\"width: 200px; max-width: 200px\">Description</th>\n            <th style=\"width: 100px; max-width: 100px\">Parameter Type</th>\n            <th style=\"width: 220px; max-width: 230px\">Data Type</th>\n            </tr>\n          </thead>\n          <tbody class=\"operation-params\">\n\n          </tbody>\n          </table>\n          ";
+  return "\n          <h4>Parameters</h4>\n          <table class='fullwidth'>\n          <thead>\n            <tr>\n            <th style=\"width: 20%\">Parameter</th>\n            <th style=\"width: 25%\">Value</th>\n            <th style=\"width: 25%\">Description</th>\n            <th style=\"width: 17.5%\">Parameter Type</th>\n            <th style=\"width: 12.5%\">Data Type</th>\n            </tr>\n          </thead>\n          <tbody class=\"operation-params\">\n\n          </tbody>\n          </table>\n          ";
   }
 
 function program7(depth0,data) {
