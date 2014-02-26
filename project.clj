de1a868803520a9cc3ecd87434f41e54da728db1
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2014-02-26 16:34:47 -0500

    Added source code for PDP

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject cfpb/api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [qu/qu-core "1.1.0-SNAPSHOT"]]
+  :main ^:skip-aot cfpb.api.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[factual/drake "0.1.3-SNAPSHOT"]]}})
