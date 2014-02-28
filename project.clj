d3d0666f947ecdf5cf2314e698407ff1e20f9409
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2014-02-28 13:51:54 -0500

    Add datasets to jar exclusions

--- project.clj
+++ project.clj
@@ -7,5 +7,8 @@
                  [qu/qu-core "1.1.0-SNAPSHOT"]]
   :main ^:skip-aot cfpb.api.core
   :target-path "target/%s"
+  :jar-exclusions [#"(^|/)\." #"datasets/.*" ]
+  :uberjar-exclusions [#"(^|/)\." #"datasets/.*"
+                       #"META-INF/.*\.SF" #"META-INF/.*\.[RD]SA"]  
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[factual/drake "0.1.3-SNAPSHOT"]]}})
